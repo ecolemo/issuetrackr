21a3c15a61d7e7805ba1b1ac71f7934453291140
Youngrok pak <pak.youngrok@gmail.com>
2011-10-26 02:37:12 +0900

    changed layout

--- issue/models.py
+++ issue/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
 class Issue(models.Model):
     writer = models.ForeignKey(User)
     title = models.CharField(max_length=200)
@@ -9,7 +13,8 @@ class Issue(models.Model):
     status = models.CharField(max_length=50, default='new')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+    tags = models.ManyToManyField(Tag)
+   
 class Comment(models.Model):
     writer = models.ForeignKey(User)
     issue = models.ForeignKey(Issue)
