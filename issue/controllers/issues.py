ffdc28c93588b17b4536ceb50de95c512914bf4a
housebba <housebba@gmail.com>
2011-10-26 18:09:32 +0900

    Tag

--- issue/controllers/issues.py
+++ issue/controllers/issues.py
@@ -2,10 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 from django.shortcuts import redirect
 from djangobp.makohelper import render_to_response
-from issue.models import Issue
+from issue.models import Issue, Tag
 
 def index(request, resource_id):
     objects = Issue.objects.order_by('-updated')
+    if 'tag' in request.GET and Tag.objects.filter(name=request.GET['tag']).exists():
+        objects = objects.filter(tags__name=request.GET['tag'])
     return render_to_response('issues/index.html', locals())
 
 @login_required
@@ -16,6 +18,10 @@ def new(request, resource_id):
 @login_required
 def create(request, resource_id):
     issue = request.user.issue_set.create(title=request.POST['title'], content=request.POST['content'])
+    tags = request.POST['tags'].split(',')
+    for tag in tags:
+        issue.tags.add(Tag.objects.get_or_create(name=tag.strip())[0])
+
     return redirect('/issues/%d' % issue.id)
 
 def show(request, resource_id):
@@ -33,6 +39,12 @@ def update(request, resource_id):
     issue.title = request.POST['title']
     issue.content=request.POST['content']
     issue.save()
+
+    issue.tags.all().delete()
+    tags = request.POST['tags'].split(',')
+    for tag in tags:
+        issue.tags.add(Tag.objects.get_or_create(name=tag.strip())[0])
+
     return redirect('/issues/%d' % issue.id)
 
 def destroy(request, resource_id):
