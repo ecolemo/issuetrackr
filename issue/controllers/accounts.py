e35da1eb044628b9720c323aa5333e803299530b
Youngrok pak <pak.youngrok@gmail.com>
2011-10-27 18:21:10 +0900

    username edit

--- issue/controllers/accounts.py
+++ issue/controllers/accounts.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth import logout as auth_logout
 from django.http import HttpResponseRedirect
 from django.core.context_processors import csrf
+from django.contrib.auth.decorators import login_required
 
 def login(request, object_id):
     csrf_token = csrf(request)['csrf_token']
@@ -16,3 +17,14 @@ def logout(request, object_id):
     """Logs out user"""
     auth_logout(request)
     return HttpResponseRedirect('/issues')
+
+@login_required
+def show(request, object_id):
+    csrf_token = csrf(request)['csrf_token']
+    return render_to_response('accounts/show.html', locals())
+
+@login_required
+def update(request, object_id):
+    request.user.username = request.POST['username']
+    request.user.save()
+    return HttpResponseRedirect('/accounts/%d/show' % request.user.id)
