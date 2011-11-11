d34b2d190c59dcee595fc222b4295d19b2a87d0d
Sukchen Kang <free1002@gmail.com>
2011-11-11 14:14:50 +0900

    admin 에서 User에 대해 username 한글 허용.

--- urls.py
+++ urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
+from django import forms
+import django
+from django.utils.translation import ugettext_lazy as _
+
 from djangobp.route import controller_resource_method_pattern, router
 import issue.controllers
 
+# admin에서 User.username 에 대해 영문&숫자 외의 문자도 허용.
+django.contrib.auth.forms.UserCreationForm.username = forms.CharField(label=_("Username"), max_length=30,
+    help_text = _("Required. 30 characters or fewer."))
 admin.autodiscover()
+# 위의 UserCreationForm.username 적용은 autodiscover 수행시엔
+# 안먹힘. 그래서 User에 대해서만 unregister & register 함.
+admin.site.unregister(django.contrib.auth.models.User)
+admin.site.register(django.contrib.auth.models.User)
 
 urlpatterns = patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'}),
