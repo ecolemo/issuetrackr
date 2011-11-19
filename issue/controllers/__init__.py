ff0f8b4c07324ecb39548b5321a9fd9b51a6600c
Sukchen Kang <free1002@gmail.com>
2011-11-19 12:08:24 +0900

    rss 적용 실험용 코드

--- issue/controllers/__init__.py
+++ issue/controllers/__init__.py
@@ -1,3 +1,23 @@
 from django.http import HttpResponseRedirect
+from django.contrib.syndication.views import Feed
+from issue.models import Issue
+
 def index(request):
     return HttpResponseRedirect('/issues')
+
+class IndexFeed(Feed):
+    title = "IssueTrac.kr site index"
+    link = "/issue/"
+    description = "Recent Issues"
+    def items(self):
+        objects = Issue.objects.order_by('-updated')
+        return objects
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.content
+
+    def item_link(self, item):
+        return '/issues/' + str(item.id)
